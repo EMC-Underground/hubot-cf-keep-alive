47cfc90bb1ac8c75dcaed6e130dfaa5087e38de8
JP Quicksall <quickjp2@users.noreply.github.com>
2016-04-27 23:16:21 -0700

    Added in the error_listener

--- driver.py
+++ driver.py
@@ -8,6 +8,14 @@ def keep_alive():
   url = os.getenv('HUBOT_URL')
   r = requests.post("{0}hubot/alive".format(url), data = json.dumps({"message":"Staying alive!","room":"{0}".format(os.getenv('HUBOT_ROOM'))}))
 
+# listener to give visibility into job completetion
+def error_listener(event):
+  if event.exception:
+    print("The job failed...{0}".format(event.exception))
+    print("{0}".format(event.traceback))
+  else:
+    print("The job worked!")
+
 if __name__ == '__main__':
   scheduler.add_job(keep_alive, 'interval', seconds=20)
   scheduler.add_listener(error_listener, events.EVENT_JOB_EXECUTED | events.EVENT_JOB_ERROR)
